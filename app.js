907baba9edc435d50ab6ec2e64e494e2e8c97139
Adarsh <adarsh800251@gmail.com>
2021-12-05 12:38:36 +0530

    committed

---
+++ app.js
@@ -0,0 +1,29 @@
+// if(process.env.NODE_ENV !== 'production'){
+//     require('dotenv').parse();
+// }
+
+const express = require('express');
+const app = express();
+const layout = require('express-ejs-layouts');
+const routes = require('./routes/routes');
+const mongoose = require('mongoose');
+
+
+app.set('view engine','ejs');
+app.set('views',__dirname + '/views');
+app.set('layout','layouts/layout');
+app.use(express.static('public'));
+app.use(layout);
+app.use('/',routes);
+
+mongoose.connect("mongodb://127.0.0.1:27017/library",{
+    useNewUrlParser:true
+}).then(()=>{
+    console.log('connected to database');
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(process.env.PORT || 5000 ,()=>{
+    console.log(`server is listening to 5000`);
+})
